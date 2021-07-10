3ab651772dc2495f58aaef05c26aea94aef7f730
Carlos Ferreira <carlosferreira@outlook.com>
2021-07-10 11:27:26 +0100

    Contract details box on Create page

--- frontend/components/CreateForm.tsx
+++ frontend/components/CreateForm.tsx
@@ -21,6 +21,12 @@ class CreateForm extends React.Component<CreateFormProps, CreateFormState>
         return (
             <form onSubmit={this.onFormSubmission.bind(this)}>
                 <div className="mb-3">
+                    <label className="form-label">Contract Type</label>
+                    <select className="form-select" defaultValue="ERC20Basic">
+                        <option value="ERC20Basic">ERC20 Basic</option>
+                    </select>
+                </div>
+                <div className="mb-3">
                     <label className="form-label">Token Name</label>
                     <input type="text" className="form-control" aria-describedby="tokenNameHelp" />
                     <div id="tokenNameHelp" className="form-text">Choose a suitable name for your new token. E.g. "Westminter Token".</div>
@@ -30,7 +36,7 @@ class CreateForm extends React.Component<CreateFormProps, CreateFormState>
                     <input type="text" className="form-control" aria-describedby="tokenSymbolHelp" />
                     <div id="tokenSymbolHelp" className="form-text">Choose a symbol for your new token. Preferably less than 5 characters long. E.g. "UOW".</div>
                 </div>
-                <button type="submit" className="btn btn-primary">Deploy Token</button>
+                <button type="submit" className="btn btn-primary">Confirm Details</button>
             </form>
         );
     }
--- frontend/pages/create.tsx
+++ frontend/pages/create.tsx
@@ -1,11 +1,41 @@
 import CreateForm from '../components/CreateForm';
+import { FaInfoCircle, FaRocket, FaExclamationTriangle } from 'react-icons/fa';
 
 const CreatePage = () =>
 {
     return (
         <>
             <h1>Create your Token</h1>
-            <CreateForm />
+            <div className="row">
+                <div className="col">
+                    <CreateForm />
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <div className="card-header">
+                            <span className="small"><FaInfoCircle /> Information message goes here...</span>
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Token Details</h5>
+                            <p className="card-text">
+                                <strong>Contract Type: </strong><span>XXX</span>
+                            </p>
+                        </div>
+                        <div className="card-footer">
+                            <div className="row">
+                                <div className="col text-start">
+                                    <span className="small"><strong><FaExclamationTriangle /> Status: </strong> Not Deployed</span>
+                                </div>
+                                <div className="col text-end">
+                                    <button className="btn btn-sm btn-primary">
+                                        <span><FaRocket /> Deploy Your Contract!</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
