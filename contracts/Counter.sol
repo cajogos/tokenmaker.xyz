e2cb075064bd8763918d77c6c25af91018ac5abf
Carlos Ferreira <carlosferreira@outlook.com>
2021-08-08 21:48:18 +0100

    Code Preparation: Solidity contracts

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,23 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+// Simple contract that can be used to test the contract creation
 contract Counter {
-
-    // Public variable of type unsigned int to keep the number of counts
     uint256 public count = 0;
 
-    // Function that increments the counter
+    // Increment the counter
     function increment() public {
         count += 1;
     }
 
-    // Function that decrements the counter
+    // Decrement the counter
     function decrement() public {
         count -= 1;
-    }
-
-    // Not necessary* Function to get the count
-    function getCount() public view returns (uint256) {
-        return count;
     }
 }
--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Altered from: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7e41bf2259950c33e55604015875b7780b6a2e63/contracts/token/ERC20/ERC20.sol
+// This is the default implementation of a ERC20 contract
+// See more: https://ethereum.org/en/developers/docs/standards/tokens/erc-20/
 
 contract ERC20 {
+    // Array of the balances for the token per address
     mapping(address => uint256) private _balances;
+    // Array of the allowances for the token per address
     mapping(address => mapping(address => uint256)) private _allowances;
+
+    // The total supply for the token
     uint256 private _totalSupply;
+
+    // The name of the token
     string private _name;
+
+    // The symbol of the token
     string private _symbol;
 
-    // This value is replaced on contract compilation
+    // This value is replaced on contract compilation (divisble by number of decimals)
     uint256 private _initialSupply = 111222333444555666777;
 
+    // The contract has 18 decimals by default
     uint8 private _decimals = 18;
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
 
+        // The creator of the contract will receive the whole initial supply
         _mint(msg.sender, _initialSupply);
     }
 
@@ -105,7 +116,6 @@ contract ERC20 {
     ) internal virtual {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
-
         uint256 senderBalance = _balances[sender];
         require(
             senderBalance >= amount,
@@ -116,13 +126,13 @@ contract ERC20 {
         }
         _balances[recipient] += amount;
 
+        // Emit the Transfer event
         emit Transfer(sender, recipient, amount);
     }
 
     // Private function to handle minting of the token (used for inital supply)
     function _mint(address account, uint256 amount) internal virtual {
         require(account != address(0), "ERC20: mint to the zero address");
-
         _totalSupply += amount;
         _balances[account] += amount;
         emit Transfer(address(0), account, amount);
@@ -136,8 +146,9 @@ contract ERC20 {
     ) internal virtual {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
-
         _allowances[owner][spender] = amount;
+
+        // Emit the Approval event
         emit Approval(owner, spender, amount);
     }
 
